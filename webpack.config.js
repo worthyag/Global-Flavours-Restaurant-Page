3a9e181cb8bc6bae1e1dfa8c03cce07431c163e9
Worthy <worthyintech@gmail.com>
2023-01-15 14:02:01 +0000

    Incorporated the webpack html plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
@@ -16,5 +17,12 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Global Flavour',
+            filename: 'index.html',
+            template: './src/template.html'
+        })
+    ]
 }
